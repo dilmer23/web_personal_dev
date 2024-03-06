0d81dc6a99ecfdd06ead5a4359762af54a17baba
Dilmer Ramirez Ramirez <dramirez@proximateapps.com>
2024-03-06 14:51:01 -0500

    Feat:navbar theme

--- src/app/view/component/navbar-view/navbar-view.component.ts
+++ src/app/view/component/navbar-view/navbar-view.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar-view',
   templateUrl: './navbar-view.component.html',
   styleUrls: ['./navbar-view.component.css']
 })
-export class NavbarViewComponent {
+export class NavbarViewComponent implements OnInit {
+  icons: boolean = true;
+  ngOnInit(): void {
+    const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const userPrefersLight = window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches;
 
-}
+    if (userPrefersDark) {
+      this.setTheme('dark');
+    }
+
+    if (userPrefersLight) {
+      this.setTheme('light');
+    }
+  }
+
+  setTheme(theme: string) {
+    document.documentElement.className = theme;
+  }
+
+  toggleTheme() {
+    const currentTheme = document.documentElement.className;
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    this.setTheme(newTheme);
+
+
+    if (newTheme === 'dark') {
+      this.icons = true;
+    } else {
+      this.icons = false;
+    }
+  }
+} 
