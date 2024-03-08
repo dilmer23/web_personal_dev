756889d25223a17639cb5c0563df281763b3b28e
Dilmer Ramirez Ramirez <dramirez@proximateapps.com>
2024-03-07 19:22:08 -0500

    feat:skill orden and icons

--- src/app/view/professional-skill/professional-skill.component.ts
+++ src/app/view/professional-skill/professional-skill.component.ts
@@ -6,45 +6,45 @@ import { Skill } from 'src/app/entity/skillmodel';
   templateUrl: './professional-skill.component.html',
   styleUrls: ['./professional-skill.component.css']
 })
-export class ProfessionalSkillComponent  {
+export class ProfessionalSkillComponent {
 
   skillRender: Skill[] = [
-    { 
-    id: 1, title: "APP de cosumo de Api RICKY AND MORTY", 
-    text: "Esta app fue desarrollada en flutter consumiendo api publica de ricky and morty , construida con clean arquiteture.",
-    img: "../../../assets/img/web/app_ricky_and_morty.png",
-    tecnologic1:"API REST",
-    tecnologic2:"FLUTTER",
-    tecnologic3:"SQLITE",
-    repository:"pruebas"
-   },
-   { 
-    id: 1, title: "Producto c node  con flutter ", 
-    text: "App móvil creada para el inventario de una empresa del sector de la construción, creada desde cero con flutter y servicios node.js",
-    img: "../../../assets/img/web/c_product_node.jpg",
-    tecnologic1:"angular.js",
-    tecnologic2:"flutter",
-    tecnologic3:"sqlite",
-    repository:"pruebas"
-   },
-   { 
-    id: 1, title: "Aplicación para la determinación ambiental E.COL", 
-    text: "Aplicación móvil del SENA para la determinación de estados hidricos del Huila por medio de macroinvertebrados  ",
-    img: "../../../assets/img/web/logo_ecol.png",
-    tecnologic1:"php",
-    tecnologic2:"JAVA",
-    tecnologic3:"ANDROID",
-    repository:"SQLITE"
-   },
-   { 
-    id: 1, title: "web portafolio dev", 
-    text: "Pagina web desarrollada desde cero , bajo un diseño visto en internet donde se visualiza mi portafolio personal",
-    img: "../../../assets/img/web/web_dilmer.jpg",
-    tecnologic1:"angular.js",
-    tecnologic2:"firabase",
-    tecnologic3:"html",
-    repository:"GITHUB"
-   }
+    //   { 
+    //   id: 1, title: "APP de cosumo de Api RICKY AND MORTY", 
+    //   text: "Esta app fue desarrollada en flutter consumiendo api publica de ricky and morty , construida con clean arquiteture.",
+    //   img: "../../../assets/img/web/app_ricky_and_morty.png",
+    //   tecnologic1:"API REST",
+    //   tecnologic2:"FLUTTER",
+    //   tecnologic3:"SQLITE",
+    //   repository:"pruebas"
+    //  },
+    {
+      id: 1, title: "MavelX con flutter ",
+      text: "App móvil fue desarrollada para una empresa del sector de la construcción para llevar el orden del personal y materiales, con el fin de tener un orden estratégico, la app fue desarrollada con flutter de Google consumiendo servicio desarrollado en node.js con express, El cual también colaboré ",
+      img: "../../../assets/img/web/c_product_node.jpg",
+      tecnologic1: "angular.js",
+      tecnologic2: "flutter",
+      tecnologic3: "sqlite",
+      repository: "pruebas"
+    },
+    {
+      id: 1, title: "Aplicación para la determinación ambiental E.COL",
+      text: "Aplicación móvil del SENA para la determinación de estados hidricos del Huila por medio de macroinvertebrados  ",
+      img: "../../../assets/img/web/logo_ecol.png",
+      tecnologic1: "php",
+      tecnologic2: "JAVA",
+      tecnologic3: "ANDROID",
+      repository: "SQLITE"
+    },
+    //  { 
+    //   id: 1, title: "web portafolio dev", 
+    //   text: "Pagina web desarrollada desde cero , bajo un diseño visto en internet donde se visualiza mi portafolio personal",
+    //   img: "../../../assets/img/web/web_dilmer.jpg",
+    //   tecnologic1:"angular.js",
+    //   tecnologic2:"firabase",
+    //   tecnologic3:"html",
+    //   repository:"GITHUB"
+    //  }
   ]
 }
 
